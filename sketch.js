6ef5d7735e701e0f34184a79e45ef4016427bcbf
Ssuman <ssuman@Ssumans-MacBook-Pro.local>
2016-12-02 21:38:19 -0500

    v0.0.1 not working but not broken so kinda cool

--- sketch.js
+++ sketch.js
@@ -1,9 +1,17 @@
+//edge blob
+//by siman li
+//runs on p5.js, jstracker, tracking.js
+//december 2016
+//v0.0.1.
+
 var capture;
 var buffer;
 var result;
 var tracker;
 var img;
 //var afterImg;
+
+//variables for threshold
 var rhi, ghi, bhi;
 var rlo, glo, blo;
 
@@ -16,6 +24,9 @@ function setTarget(r, g, b, range) {
   bhi = b + range, blo = b - range;
 }
 
+
+//function for setting up() initial conditions
+//part of the p5.js libraries
 function setup() {
   createCanvas(w, h);
   background(100);
@@ -23,59 +34,68 @@ function setup() {
   capture.size(w, h);
   capture.hide();
   buffer = new jsfeat.matrix_t(w, h, jsfeat.U8C1_t);
-  button=createButton('snap');
+  button = createButton('snap');
   // button.mousePressed(takesnap);
     setTarget(255, 255, 255); // by default track white
-  tracking.ColorTracker.registerColor('match', function(r, g, b) {
-    if(r <= rhi && r >= rlo &&
-       g <= ghi && g >= glo &&
-       b <= bhi && b >= blo) {
-      return true;
-    }
-    return false;
-  });
-   tracker = new tracking.ColorTracker(['match']);
-  tracker.minDimension = 20; // make this smaller to track smaller objects
-  capture.elt.id = 'p5video';
-  img.id = ("p5img");
+  // tracking.ColorTracker.registerColor('match', function(r, g, b) {
+  //   if(r <= rhi && r >= rlo &&
+  //      g <= ghi && g >= glo &&
+  //      b <= bhi && b >= blo) {
+  //     return true;
+  //   }
+  //   return false;
+  // });
+  //  tracker = new tracking.ColorTracker(['match']);
+  // tracker.minDimension = 20; // make this smaller to track smaller objects
+  // capture.elt.id = 'p5video';
+  //img.id = ("p5img");
   // tracking.track('p5img', tracker, { camera: true });
-  tracking.track('p5img', tracker, { camera: true });
-  tracker.on('track', function(event) {
-    clear();
-    strokeWeight(4);
-    stroke(255, 0, 0);
-    noFill();
-    event.data.forEach(function(r) {
-      rect(r.x, r.y, r.width, r.height);
-    });
-  });
+  // tracking.track('p5img', tracker, { camera: true });
+  // tracker.on('track', function(event) {
+  //   clear();
+  //   strokeWeight(4);
+  //   stroke(255, 0, 0);
+  //   noFill();
+  //   event.data.forEach(function(r) {
+  //     rect(r.x, r.y, r.width, r.height);    });
+  //});
 }
+
+//draw() function runs after setup() on a loop
+//part of the p5.js library
+function draw() {
+//  // image(capture, 0,0, 640, 490);
+//    if(mouseIsPressed &&
+//     mouseX > 0 && mouseX < width &&
+//     mouseY > 0 && mouseY < height) {
+//     capture.loadPixels();
+//     target = capture.get(mouseX, mouseY);
+//     setTarget(target[0], target[1], target[2]);
+//   }
+//   img.loadPixels();
+//   if(img.pixels.length > 0) { // don't forget this!
+//     var blurSize = 6;
+//     var lowThreshold = mouseX;
+//     var highThreshold = mouseY;
+//     console.log(mouseX ," ", mouseY);
+//     jsfeat.imgproc.grayscale(img.pixels, w, 490, buffer);
+//     jsfeat.imgproc.gaussian_blur(buffer, buffer, blurSize, 0);
+//     jsfeat.imgproc.canny(buffer, buffer, lowThreshold, highThreshold);
+//     result = jsfeatToP5(buffer, result);
+//     image(result, 0, 0, 640, 500);
+//   }
+}
+
+
+
 function takesnap(){
-img=image(result,0,500);
+  img = image(result, 0, 500);
 }
+
 function keyTyped() {
   if (key === 'a') {
-takesnap();  } } //when the key is pressed==a, take a snapshot.
-
-function draw() {
- // image(capture, 0,0, 640, 490);
-   if(mouseIsPressed &&
-    mouseX > 0 && mouseX < width &&
-    mouseY > 0 && mouseY < height) {
-    capture.loadPixels();
-    target = capture.get(mouseX, mouseY);
-    setTarget(target[0], target[1], target[2]);
-  }
-  img.loadPixels();
-  if(img.pixels.length > 0) { // don't forget this!
-    var blurSize = 6;
-    var lowThreshold = mouseX;
-    var highThreshold = mouseY;
-    console.log(mouseX ," ", mouseY);
-    jsfeat.imgproc.grayscale(img.pixels, w, 490, buffer);
-    jsfeat.imgproc.gaussian_blur(buffer, buffer, blurSize, 0);
-    jsfeat.imgproc.canny(buffer, buffer, lowThreshold, highThreshold);
-    result = jsfeatToP5(buffer, result);
-    image(result, 0, 0, 640, 500);
-  }
+      takesnap();
+    }
 }
+
+//when the key is pressed==a, take a snapshot.
